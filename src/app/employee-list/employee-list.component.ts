bcd0d4c1539a7de3bcffc2912edf6185d79a6317
MFRADJ <mohamed-fradj@outlook.com>
2024-04-14 19:34:49 +0200

    exemple of employees list developed

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -12,7 +12,41 @@ export class EmployeeListComponent implements OnInit {
   constructor() {
    }
 
+<<<<<<< Updated upstream
   ngOnInit(): void {
   }
 
+=======
+ ngOnInit(): void {
+  this.employees = [
+    {
+      "id": 1002,
+      "datOfBirth": new Date("1994-05-18"),
+      "departement": "marsaille",
+      "position": "Tech",
+      "salary": 50000,
+      "email": "tom.hanks@gmail.com",
+      "lastname": "hanks",
+      "firstname": "tom"
+    },
+    {
+      "id": 1003,
+      "datOfBirth": new Date("1994-05-18"),
+      "departement": "Paris",
+      "position": "Eng",
+      "salary": 60000,
+      "email": "ali.abid@gmail.com",
+      "lastname": "abid",
+      "firstname": "ali"
+    }
+  ]
+  //  this.getEmployees();
+  }
+  //private getEmployees(){
+   // this.employeeService.getEmployeesList().subscribe(data => {
+    //  this.employees = data
+   // }
+ // )
+  //}
+>>>>>>> Stashed changes
 }
