c700ae3b30c6341473d5b5a9656842a5813218f3
MFRADJ <mohamed-fradj@outlook.com>
2024-04-15 00:07:24 +0200

    EmployeeService work correctly

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../employee';
+import { Employee } from '../model/employee';
+import { EmployeeService } from '../employee.service';
+import { error } from 'console';
+
 
 @Component({
   selector: 'app-employee-list',
@@ -7,12 +10,30 @@ import { Employee } from '../employee';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-  employees: Employee[] = [];
+  employees: Employee[] = [] ;
+  loading: boolean = true;
 
   constructor(private employeeService: EmployeeService) {
+   
    }
 
   ngOnInit(): void {
+    this.getEmployees();
+  }
+  private getEmployees() {
+    this.employeeService.getEmployeesList().subscribe( {
+      next: (data) => {
+        this.employees = data;
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Erreur lors de la récupération des employés:', error);
+        this.loading = false;
+      },
+      complete: () => {
+        console.log('Opération de récupération des employés complétée.');
+      }
+    });
   }
 
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Employee } from './employee';
+import { Employee } from './model/employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private baseUrl  = "http://localhost:8080/api/v1/employees/"
+  private baseUrl  = "http://localhost:8080/api/v1/employees"
   constructor(private httpClient: HttpClient) {
     
    }
